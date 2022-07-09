76597e1660ea43f92e3161679bdece6af5aa198f
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-07-09 22:18:01 +0530

    Create Codes.java

---
+++ 09-July-2022/Codes.java
@@ -0,0 +1,66 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	     Scanner scn=new Scanner(System.in);
+	     int marks=scn.nextInt();
+	     int classs=scn.nextInt();
+	     
+	     
+	   
+	   
+	   if(classs==8){
+	       System.out.println("My class is 8th");
+	       if(marks>=90 ){
+	         System.out.println("My Grade is A");
+	     }
+	     else if(marks>=80 && marks<90 ){
+	         System.out.println("My Grade is B");
+	     }
+	     else if(marks>=70 && marks<80  ){
+	         System.out.println("My Grade is C");
+	     }
+	     else if(marks>=60 && marks<70 ){
+	         System.out.println("My Grade is D");
+	     }else  {
+	         System.out.println("My Grade is E");
+	     }
+	       
+	   }else if(classs==9){
+	       System.out.println("My class is 9th");
+	       if(marks>=90 ){
+	         System.out.println("My Grade is A");
+	     }
+	     else if(marks>=80 && marks<90 ){
+	         System.out.println("My Grade is B");
+	     }
+	     else if(marks>=70 && marks<80  ){
+	         System.out.println("My Grade is C");
+	     }
+	     else if(marks>=60 && marks<70 ){
+	         System.out.println("My Grade is D");
+	     }else  {
+	         System.out.println("My Grade is E");
+	     }
+	       
+	   }else if(classs==10){
+	       System.out.println("My class is 10th");
+	       if(marks>=90 ){
+	         System.out.println("My Grade is A");
+	     }
+	     else if(marks>=80 && marks<90 ){
+	         System.out.println("My Grade is B");
+	     }
+	     else if(marks>=70 && marks<80  ){
+	         System.out.println("My Grade is C");
+	     }
+	     else if(marks>=60 && marks<70 ){
+	         System.out.println("My Grade is D");
+	     }else  {
+	         System.out.println("My Grade is E");
+	     }
+	       
+	   }
+
+	}
+}
