30814e9f0bcbc394d4e6bfd8f6ed2a8f495b6fc9
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-07-15 10:38:34 +0530

    Create GKSTR02 Concatenate_Two_Strings.java

---
+++ July KS Class Challenges/GKSTR02 Concatenate_Two_Strings.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn=new Scanner(System.in);
+        String str1=scn.next();
+        String str2=scn.next();
+        
+        System.out.println(str1+str2);
+    }
+}
