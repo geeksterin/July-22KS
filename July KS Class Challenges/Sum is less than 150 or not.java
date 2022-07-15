ca0ea3388b9d64a3953d9fa81cf28db41987e235
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-07-15 10:21:03 +0530

    Create Sum is less than 150 or not.java

---
+++ July KS Class Challenges/Sum is less than 150 or not.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn=new Scanner(System.in);
+        int x=scn.nextInt();
+        int y=scn.nextInt();
+        int z=scn.nextInt();
+        
+        int sum=x+y+z;
+        
+        if(sum<150){
+            System.out.println(true);
+        }else {
+            System.out.println(false);
+        }
+    }
+}
