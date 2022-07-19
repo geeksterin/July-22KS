49a73799123c83d9f71c4b8ff39825286b996bda
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-07-19 09:23:24 +0530

    Create GKSTR11 Multiple Of 7.java

---
+++ July KS Class Challenges/GKSTR11 Multiple Of 7.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn=new Scanner(System.in);
+        
+        int n=scn.nextInt();
+        
+        // for(int i=0;i<=n;i+=7){
+        //     System.out.print(i+"\t");
+        // }
+        
+        int i=0;
+        
+        while(i<=n){
+            System.out.print(i+"\t");
+            i+=7;
+        }
+    }
+}
