a113556ba8ef0be1252f017b706f9a403b104f2f
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-07-19 09:22:45 +0530

    Create Print table of 4.java

---
+++ July KS Class Challenges/Print table of 4.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int i=1;
+        while(i<=10){
+            System.out.println("4x"+i+"="+4*i);
+            i++;
+            
+        }
+    }
+}
