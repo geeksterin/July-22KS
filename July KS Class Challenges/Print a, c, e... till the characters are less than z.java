0f1d1f8cc88a36c9c8c43f05bb52e43b7baf45cd
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-07-24 16:40:49 +0530

    Create Print a, c, e... till the characters are less than z.java

---
+++ July KS Class Challenges/Print a, c, e... till the characters are less than z.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        // for(char i='a' ;i<'z' ;i+=2){
+        //     System.out.println(i);
+        // }
+        
+        char i='a';
+        
+        while(i<'z'){
+            System.out.println(i);
+            i+=2;
+        }
+    }
+}
