4a4b6d357753c5a04b8af09c238d3b03f66417de
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-07-30 22:01:25 +0530

    Create Pattern 7 - Print a hollow m by n star rectangle..java

---
+++ July KS Class Challenges/Pattern 7 - Print a hollow m by n star rectangle..java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn=new Scanner(System.in);
+        
+        int m=scn.nextInt();
+        int n=scn.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i==1 || i==n || j==1 || j==m){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
