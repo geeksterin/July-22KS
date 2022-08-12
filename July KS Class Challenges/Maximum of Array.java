a3539842ed0d90cdfea6ab26c44bb233ae40e1c1
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-08-12 13:31:22 +0530

    Create Maximum of Array.java

---
+++ July KS Class Challenges/Maximum of Array.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static int maxOfArray(int arr[]){
+        
+        int n=arr.length;
+        int max=arr[0];
+        
+        for(int i=1;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        
+        return max;
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        
+        int arr[]=new int [n];
+        
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+        
+        int ans=maxOfArray(arr);
+        
+        System.out.println(ans);
+    }
+}
