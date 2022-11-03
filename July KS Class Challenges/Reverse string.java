3650ad9d7b3daa6b2621c255af5f648e3681677c
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-11-03 22:07:35 +0530

    Create Reverse string.java

---
+++ July KS Class Challenges/Reverse string.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn=new Scanner(System.in);
+        
+        String str=scn.nextLine();
+        
+        Stack<Character>st=new Stack<>();
+        
+        
+        for(int i=0;i<str.length();i++){
+            st.push(str.charAt(i));
+        }
+        
+        
+        while(st.size()>0){
+            System.out.print(st.pop());
+        }
+    }
+}
